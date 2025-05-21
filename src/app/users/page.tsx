4f9ecb30d1e6095a3c0cdd9f5f2434fa7f82af7a
Md Yousuf Ali <yousuf3usb@gmail.com>
2025-05-21 22:50:15 +0600

    feat: use flex-wrap

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -4,7 +4,7 @@ export default async function UsersPage() {
   const res = await fetch('https://jsonplaceholder.typicode.com/users');
   const users = await res.json();
   return (
-    <div>
+    <div className="flex flex-wrap justify-center items-stretch">
       {users.map((user: any) => (
         <UserCard key={user.id} user={user} />
       ))}
